bad2f4c520387c792d185ac3036de9744100ba29
paulo-fs <jr.paulo87@gmail.com>
2022-10-22 12:56:53 -0300

    feat: define colors and typefaces of dark theme

--- src/shared/styles/theme/defaultTheme.ts
+++ src/shared/styles/theme/defaultTheme.ts
@@ -1,3 +1,51 @@
 export const darkTheme = {
-   
+   colors: {
+      yellow: '#FFB706',
+      yellowT06: 'rgba(255, 183, 6, 0.6)',
+      yellowT03: 'rgba(255, 183, 6, 0.3)',
+      yellowT01: 'rgba(255, 183, 6, 0.1)',
+
+      orange: 'rgba(243, 81, 35, 1)',
+      orangeT06: 'rgba(243, 81, 35, 0.6)',
+      orangeT03: 'rgba(243, 81, 35, 0.3)',
+      orangeT01: 'rgba(243, 81, 35, 0.1)',
+
+      blue: 'rgba(2, 164, 239, 1)',
+      blueT06: 'rgba(2, 164, 239, 0.6)',
+      blueT03: 'rgba(2, 164, 239, 0.3)',
+      blueT01: 'rgba(2, 164, 239, 0.1)',
+
+      green: 'rgba(127, 185, 2, 1)',
+      greenT06: 'rgba(127, 185, 2, 0.6)',
+      greenT03: 'rgba(127, 185, 2, 0.3)',
+      greenT01: 'rgba(127, 185, 2, 0.1)',
+
+      red: 'rgba(217, 43, 43, 1)',
+      redT06: 'rgba(217, 43, 43, 0.6)',
+      redT03: 'rgba(217, 43, 43, 0.3)',
+      redT01: 'rgba(217, 43, 43, 0.1)',
+
+      white: 'rgba(240, 244, 247, 1)',
+      whiteT06: 'rgba(240, 244, 247, 0.6)',
+      whiteT03: 'rgba(240, 244, 247, 0.3)',
+      whiteT01: 'rgba(240, 244, 247, 0.1)',
+
+      black: 'rgba(17, 17, 17, 0.8)',
+
+      darkBlue: 'rgba(23, 24, 39, 1)',
+      darkBlueT08: 'rgba(23, 24, 39, 0.8)',
+      darkBlueT03: 'rgba(23, 24, 39, 0.3)',
+      darkBlueT01: 'rgba(23, 24, 39, 0.1)',
+   },
+
+   fonts: {
+      heading1: '600 3rem/120% Poppins, sans-serif',
+      heading2: '600 2.25rem/110% Poppins, sans-serif',
+      heading3: '600 2rem/120% Poppins, sans-serif',
+      heading4: '500 1.5rem/120% Poppins, sans-serif',
+      body1: '400 1.25rem/140% Poppins, sans-serif',
+      body2: '400 1rem/150% Poppins, sans-serif',
+      body3: '400 0.75rem/150% Poppins, sans-serif',
+      inputs: 'italic upper-case 500 1rem/120% Poppins, sans-serif'
+   }
 }
