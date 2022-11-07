d865c2eb037df6d2811af76c131d5a605ea64885
paulo-fs <jr.paulo87@gmail.com>
2022-11-07 14:13:53 -0300

    fix: some changes in svg images and styles

--- src/components/MyNotationsTable/index.tsx
+++ src/components/MyNotationsTable/index.tsx
@@ -1,6 +1,20 @@
-import { Table, TableFooter } from './styles';
+import { useState } from 'react';
+import { ReactSVG } from 'react-svg';
+
+import { PrimaryBtn } from '@/components';
+
+import iconPrev from '@/shared/assets/icons/Arrow-LeftCircle.svg';
+import iconNext from '@/shared/assets/icons/Arrow-RightCircle.svg';
+
+import { ChangePageButton, RowInfos, Table, TableFooter } from './styles';
 
 export default function MyNotationsTable() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpenCloseInfos() {
+    setIsOpen((prevState) => !prevState);
+  }
+
   return (
     <>
       <Table>
@@ -16,7 +30,7 @@ export default function MyNotationsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr onClick={handleOpenCloseInfos}>
             <td>10/10/2022</td>
             <td>09:00 - 12:00</td>
             <td>03:00</td>
@@ -25,6 +39,24 @@ export default function MyNotationsTable() {
             <td>Desenvolvimento</td>
             <td>Pendente</td>
           </tr>
+          <RowInfos>
+            <td colSpan={5}>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Distinctio, ratione. Repellat sit accusantium alias impedit,
+                eveniet quam sequi magni ex quod quidem ratione pariatur
+                molestiae quasi quos debitis dolore eos!
+              </p>
+            </td>
+            <td colSpan={2}>
+              <div>
+                <input type="checkbox" />
+                <span>Contabilizado</span>
+              </div>
+              <PrimaryBtn>Editar</PrimaryBtn>
+            </td>
+          </RowInfos>
+
           <tr>
             <td>10/10/2022</td>
             <td>09:00 - 12:00</td>
@@ -37,7 +69,18 @@ export default function MyNotationsTable() {
         </tbody>
       </Table>
       <TableFooter>
-        <p>Páginas</p>
+        <ChangePageButton>
+          <ReactSVG src={iconPrev} />
+        </ChangePageButton>
+        <div>
+          <button className="page">1</button>
+          <button className="page">2</button>
+          <button className="page">3</button>
+          <button className="page">4</button>
+        </div>
+        <ChangePageButton>
+          <ReactSVG src={iconNext} />
+        </ChangePageButton>
       </TableFooter>
     </>
   );
--- src/components/MyNotationsTable/styles.ts
+++ src/components/MyNotationsTable/styles.ts
@@ -1,3 +1,4 @@
+import { ReactSVG } from "react-svg"
 import styled from "styled-components"
 
 export const Table = styled.table`
@@ -49,7 +50,7 @@ export const Table = styled.table`
    `
 
 export const TableFooter = styled.div`
-   padding: .7rem 1rem;
+   padding: .7rem 1.5rem;
    margin: 0 2px;
    border-radius: 0 0 20px 20px;
    color: ${({theme}) => theme.colors.darkBlue};
@@ -57,4 +58,40 @@ export const TableFooter = styled.div`
 
    display: flex;
    justify-content: flex-end;
+
+   button{
+      border: none;
+      background-color: transparent;
+   }
+
+   .page{
+      padding: .5rem;
+      font: ${({theme}) => theme.fonts.body2};
+      color: ${({theme}) => theme.colors.darkBlue};
+   }
 `
+
+export const RowInfos = styled.tr`
+   width: 100%;
+   background-color: ${({theme}) => theme.colors.whiteT01};
+
+   button{
+      width: 100%;
+      margin-top: 2rem;
+   }
+`
+
+export const ChangePageButton = styled.button`
+   border: none;
+   background-color: transparent;
+
+   svg path{
+      stroke: ${({theme}) => theme.colors.darkBlue};
+   }
+
+   &:hover{
+      svg path{
+         stroke: ${({theme}) => theme.colors.darkBlueT03};
+      }
+   }
+`;
